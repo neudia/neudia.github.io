5642d90154147eb497f6e9a6e409513d7bfdc318
chanmioh <chanmioh@gmail.com>
2022-11-19 11:26:28 -0500

    Attempt fix button issue

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -119,7 +119,7 @@ const Navbar = () => {
         {navItems}
 
         {/* Toggles drawer. */}
-        <MenuButton onClick={() => setIsOpen(!isOpen)}>
+        <MenuButton type="button" onClick={() => setIsOpen(!isOpen)}>
           <GiHamburgerMenu size={32} />
         </MenuButton>
       </NavContent>
